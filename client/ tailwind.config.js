046cc5d9644a5f4b246358e8b951872b503a08a6
Arman <armanghazaryan2409@gmail.com>
2025-03-17 23:20:20 +0400

    feat: add Tailwind config

---
+++ client/ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        colors: {
+            primary: '#42e329',
+            secondary: '#f7fafc',
+            gre: '#f7fafc',
+        },
+    },
+    plugins: [],
+};
